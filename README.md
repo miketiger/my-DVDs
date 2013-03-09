eb7e7bfcf9a8004183b98da7017a661d155c0543
Mike Lyons <mtiger@lap2.(none)>
2013-03-09 15:22:55 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# First Ruby on Rails app
+
+This is the first application for the [*Ruby on Rails Tutorial*](url goes here)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyDVDs::Application.config.secret_token = '34d3f4bbe90f39dd9fc40d23682adbb9898e4fd2e5e943cf5b24bfade003302a882863fab4d1743024e0f5e660ef56bb572ca03a5582247b343484edf2a535f2'
